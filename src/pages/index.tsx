c0038e6b2d4a8c7001b46856372e449df694c197
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2021-08-08 16:01:36 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,3 +5,5 @@ export const Index: FC = () => {
     <div className="py-1 px-3 w-full h-9 dark:text-white bg-black rounded border focus:border focus:ring-4 focus:ring-sky-800 focus:ring-offset-2 focus:ring-offset-sky-300 appearance-none focus:outline-none" />
   );
 };
+
+export default Index;

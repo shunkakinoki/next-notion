52b237ba9989de854e76b8f9067cdfb85f655279
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-08 14:59:22 -0700

    feat: ini next notion

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+
+export const Index: FC = () => {
+  return (
+    <div className="py-1 px-3 w-full h-9 dark:text-white bg-black rounded border focus:border focus:ring-4 focus:ring-sky-800 focus:ring-offset-2 focus:ring-offset-sky-300 appearance-none focus:outline-none" />
+  );
+};
